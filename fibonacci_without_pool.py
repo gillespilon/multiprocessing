55a225a419bd1bf44bad57b8c935dfcbcfec3473
Gilles Pilon <gilles.pilon@gillespilon.ca>
2019-06-21 10:58:42 -0500

    Fixed all notes per flake8

--- fibonacci_without_pool.py
+++ fibonacci_without_pool.py
@@ -7,29 +7,32 @@ That is, not multiprocessing
 
 
 import time
-def fibonacci_sequence_of(num):
-	first_number = 0
-	second_number = 1
-	num = int(num)
-	if num == 0:
-		print ("Fibonacci of {} is {}".format(num,num))
-	elif num ==1:
-		print ("Fibonacci of {} is {}".format(num,num))
-	else:
-		for i in range(2,num):
-			new_number = first_number + second_number
-			first_number = second_number
-			second_number = new_number
-		print ("Fibonacci of {} is {}".format(num,num))
+
+
+def fibonacci_sequence(number):
+    first_number = 0
+    second_number = 1
+    number = int(number)
+    if number == 0:
+        print(f'Fibonacci of {number} is {number}')
+    elif number == 1:
+        print(f'Fibonacci of {number} is {number}')
+    else:
+        for item in range(2, number):
+            new_number = first_number + second_number
+            first_number = second_number
+            second_number = new_number
+        print(f'Fibonacci of {number} is {number}')
+
 
 if __name__ == '__main__':
-
-	input_number = input("Provide comma-seperated-values for multiple values \nFabonacci of : ")
-	input_values=[]
-	input_values = input_number.split(",")
-	toc = time.time()
-	for i in input_values:
-		fibonacci_sequence_of(i)
-	tic = time.time()
-	time_taken=round((tic-toc)*1000, 1)
-	print ("It takes {} milli-seconds to calculate the fibonacci of {} concurrently".format(time_taken,input_number))
+    input_number = input(f'Provide number to calculate: ')
+    input_value = []
+    input_value = input_number.split(f',')
+    toc = time.time()
+    for item in input_value:
+        fibonacci_sequence(item)
+    tic = time.time()
+    time_taken = round((tic-toc)*1000, 1)
+    print(f'It takes {time_taken} milli-seconds to calculate the',
+          f'fibonacci of {input_number} concurrently')
